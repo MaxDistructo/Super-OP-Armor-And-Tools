705568ea95fc771678334cce293abf3932a9c53f
MaxDistructo <micanjvollan@gmail.com>
2015-11-18 12:00:52 -0600

    Super_OP_Armor+Tools.js 0.0.1
    Easter Eggs And Start of Sword Attack Making

--- Super_OP_Armor+Tools.js
+++ Super_OP_Armor+Tools.js
@@ -1,3 +1,25 @@
+//Super OP Armor and Tools//
+//***By:MaxDistructo***//
 ModPE.setItem(399,"nether_star",0,"Nether Star");
 Item.addShapedRecipe(399,1,0,[" a ","aba"," a "],["a",155,0,"b",264,0]);
 //Creation of the Nether Star//
+ModPE.setItem(1500,"blaze_rod",0,"Nether Star Sword:WIP");
+Item.setMaxDamage(1500,5000)
+var damage = 20
+function attackHook(a,v){
+  if (Player.getCarriedItem() == 1500)
+    if ([Player.getCarriedItemData() == 5000)
+    Entity.setHealth(victim,Entity.getHealth (victim) - damage);
+    Entity.getCarriedItem(a,1500),
+    Player.getCarriedItemCount(),
+    Player.getCarriedItemData()+1
+      else
+      
+  
+  
+}
+
+//Easter Eggs!//
+function chatHook(fuck){Stop Swearing!}
+function chatHook(bitch){Stop Swearing!}
+}
