fd2e2862e76e75ee2616c3ec44564705815e4b31
MaxDistructo <micanjvollan@gmail.com>
2015-11-19 08:20:42 -0600

    Only 16 in a stack of Nether Star

--- Super_OP_Armor+Tools.js
+++ Super_OP_Armor+Tools.js
@@ -1,7 +1,7 @@
 //Super OP Armor and Tools//
 //***By:MaxDistructo***//
 ModPE.setItem(399,"nether_star",0,"Nether Star");
-Item.addShapedRecipe(399,1,0,[" a ","aba"," a "],["a",155,0,"b",264,0]);
+Item.addShapedRecipe(399,1,0,[" a ","aba"," a "],["a",155,0,"b",264,0],16);
 //Creation of the Nether Star//
 ModPE.setItem(1500,"blaze_rod",0,"Nether Star Sword:WIP");
 Item.setMaxDamage(1500,5000)
